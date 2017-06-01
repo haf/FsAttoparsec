b13fe7791d787853fa953aaada7fe95b95dfaece
Henrik Feldt <henrik@haf.se>
2017-06-01 10:08:14 +0200

    BinaryParser tests revamped

--- FsAttoparsec.Tests/BinaryParser.fs
+++ FsAttoparsec.Tests/BinaryParser.fs
@@ -1,17 +1,17 @@
-﻿namespace Attoparsec.Tests
+﻿module Attoparsec.Tests.BinaryParser
 
+open Expecto
+open Expecto.Flip
 open FsCheck
 open System
 open Attoparsec
 open Attoparsec.Binary
 
-[<TestFixture>]
-module BinaryParserTest =
+module Props =
 
   module Array =
     let cons b xs = Array.append [| b |] xs
 
-  [<Property>]
   let ``satisfy`` b xs =
     let actual =
       Array.cons b xs
@@ -19,37 +19,30 @@ module BinaryParserTest =
       |> ParseResult.option
     actual = Some b
 
-  [<Property>]
-  let ``byte`` b xs =
+  let byte b xs =
     let actual =
       Array.cons b xs
       |> parse (pbyte b)
       |> ParseResult.option
     actual = Some b
 
-  [<Property>]
   let ``anyByte`` xs =
     let p = (parse anyByte xs).Option
     match List.ofArray xs with | [] -> p = None | x::_ -> p = Some x
 
-  [<Property>]
   let ``notByte`` b xs =
     ((not <| Array.isEmpty xs) ==>
       lazy (let v = xs.[0] in (parse (notByte b) xs).Option = (if v = b then None else Some v)))
 
-  [<Test>]
-  [<Property>]
   let ``bytes`` s t =
     (parse (bytes s) (Array.append s t)).Option = Some (BinaryArray.ofArray s)
 
-  [<Property>]
   let ``takeCount`` k s =
     (k >= 0) ==> lazy (
       match (parse (take k) s).Option with
       | None -> k > Array.length s
       | Some _ -> k <= Array.length s)
 
-  [<Property>]
   let ``takeWhile `` b xs =
     let s = BinaryArray.ofArray xs
     let (h, t) = BinaryArray.span ((=) b) s
@@ -61,8 +54,7 @@ module BinaryParserTest =
     })
     |> (=) (Choice1Of2 (h, t))
 
-  [<Property>]
-  let ``takeWhile1 `` b xs =
+  let takeWhile1 b xs =
     let sp = Array.cons b xs
     let s = BinaryArray.ofArray sp
     let (h, t) = BinaryArray.span (fun x -> x <= b) s
@@ -74,19 +66,16 @@ module BinaryParserTest =
     })
     |> (=) (Choice1Of2 (h, t))
 
-  [<Test>]
   let ``takeWhile1 empty`` () =
     [||]
     |> parse (Binary.takeWhile1 (fun _ -> true))
     |> ParseResult.option
-    |> should equal None
+    |> Expect.isNone "No input means nothing can be parsed"
 
-  [<Property>]
   let ``endOfInput`` s =
     s |> parseOnly endOfInput = (if Array.isEmpty s then Choice1Of2 () else Choice2Of2 "endOfInput")
 
-  [<Property>]
-  let ``getPosition `` b xs =
+  let getPosition b xs =
     let sp = Array.cons b xs
     let s = BinaryArray.ofArray sp
     let expected = BinaryArray.takeWhile (fun x -> x <= b) s |> BinaryArray.length
@@ -96,3 +85,19 @@ module BinaryParserTest =
       return! getPosition
     })
     |> (=) (Choice1Of2 expected)
+
+
+[<Tests>]
+let tests =
+  testList "binary parser" [
+    testProperty "satisfy" Props.satisfy
+    testProperty "byte" Props.byte
+    testProperty "anyByte" Props.anyByte
+    testProperty "notByte" Props.notByte
+    testProperty "bytes" Props.notByte
+    testProperty "takeCount" Props.takeCount
+    testProperty "takeWhile1" Props.takeWhile1
+    testCase "takeWhile1 empty" Props.``takeWhile1 empty``
+    testProperty "endOfInput" Props.endOfInput
+    testProperty "getPosition" Props.getPosition
+  ]
